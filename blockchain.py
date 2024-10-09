bf49f2afb212952f254120118df352ad6b7586ff
asifmanan <asifmanan@gmail.com>
2024-10-09 02:13:19 +0100

    This removes the minning functions from the blockchain.py to only include the Blockchain class in this file. Also, fixes some syntax errors in the code

--- blockchain.py
+++ blockchain.py
@@ -7,19 +7,18 @@
 import datetime
 import hashlib
 import json
-from flask import Flask, jsonify
 
 # Part 1 - Building a Blockchain 
 class Blockchain:
   def __init__(self):
     self.chain = []
-    self.create_block(proof = 1, previous_block = '0')
+    self.create_block(proof = 1, previous_hash = '0')
 
   def create_block(self, proof, previous_hash):
-    block = {'index', self.chain(len) + 1,
-             'timestamp', str(datetime.datetime.now()),
-             'proof', proof,
-             'previous_hash', previous_hash }
+    block = {'index': len(self.chain) + 1,
+             'timestamp': str(datetime.datetime.now()),
+             'proof': proof,
+             'previous_hash': previous_hash }
     self.chain.append(block)
     return block
   
@@ -41,7 +40,7 @@ class Blockchain:
   # For blockchain validation 
   def hash(self, block):
     encoded_block = json.dumps(block, sort_keys=True).encode()
-    return hashlib.sha256(encoded_block.hexdigest())
+    return hashlib.sha256(encoded_block).hexdigest()
   
   def is_chain_valid(self, chain):
     previous_block = chain[0]
@@ -58,4 +57,3 @@ class Blockchain:
       previous_block = chain[block_index]
       block_index += 1
     return True
-    
