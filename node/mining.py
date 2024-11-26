8189b420d5919d45a48efdee53e8e22e0d73dba5
asifmanan <asifmanan@gmail.com>
2024-11-26 14:36:33 +0000

    Updates miner and mining to execute mining instructions, run_node script is added to coordinate the mining process. Uses a daemon process to run the mining node

--- node/mining.py
+++ node/mining.py
@@ -1,23 +1,47 @@
-# Reference
-from blockchain.blockchain import Blockchain
+
 class Mining:
   def __init__(self, blockchain, node_address, owner_id):
+    """
+      Initialize the Mining instance with a blockchain, node address, and owner ID.
+    """
     self.blockchain = blockchain
     self.node_address = node_address
     self.owner_id = owner_id
+
   def mine_block(self):
-    previous_block = self.blockchain.get_last_block()
-    previous_proof = previous_block['proof']
-    proof = self.blockchain.proof_of_work(previous_proof)
-    previous_hash = self.blockchain.hash(previous_block)
-    self.blockchain.record_transactions(sender=self.node_address, receiver=self.owner_id,amount=10)
-    block = self.blockchain.create_block(proof, previous_hash)
-    response = {
-                'message': 'Congragulations! A new block was created',
-                'index': block['index'], 
-                'timestamp': block['timestamp'], 
-                'proof': block['proof'], 
-                'previous_hash': block['previous_hash'] , 
-                'transactions': block['transactions']
-                }
-    return response
+    """
+      Mines a new block and adds it to the local blockchain.
+    """
+    try:
+      # Get the last block from the block chain. 
+      last_block = self.blockchain.get_last_block()
+      last_proof = last_block.proof
+
+      # Perform proof of work
+      proof = self.blockchain.proof_of_work(last_proof)
+      # last_block_hash = self.blockchain.hash(last_block)
+
+      # Record the reward transaction
+      self.blockchain.record_transactions(
+        sender=self.node_address, receiver=self.owner_id,amount=10
+        )
+      
+      # Create a new block 
+      block = self.blockchain.create_block(proof, last_block.hash)
+      print("block index = "+ block.index)
+
+      # Returning a structured message 
+      response = {
+                  'message': 'A new block was created',
+                  'index': block.index, 
+                  'timestamp': block.timestamp, 
+                  'proof': block.proof, 
+                  'previous_hash': block.previous_hash , 
+                  'transactions': block.transactions
+                  }
+    except Exception as e:
+      response = {
+                  'message': f'An error occured while mining the block: {e}',
+                  'index': None
+                  }
+    return block
---
+++ node/run_node.py
@@ -0,0 +1,29 @@
+import time
+from blockchain.blockchain import Blockchain
+from node.mining import Mining
+from node.miner import Miner
+
+if __name__ == '__main__':
+  # Create a blockchain instance
+  blockchain = Blockchain()
+
+  # Create a mining instance 
+  mining_instance = Mining(blockchain, node_address="my_node", owner_id="owner_x")
+
+  # Create a miner instance 
+  miner = Miner(mining_instance)
+
+  try: 
+    # Start the miner 
+    print("Starting the miner...")
+    miner.start_mining() 
+    time.sleep(10)
+    
+  except KeyboardInterrupt: 
+    print("Interrup received! Stopping Miner...")
+    # And let it run for a while 
+  finally:
+    print("Stopping the miner")
+    miner.stop_mining()
+
+
